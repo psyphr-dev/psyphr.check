d837cf2fa553c1813974302a1883e88da9331549
Siqi Zhang <iqis.gnahz@gmail.com>
2020-02-13 15:46:43 -0500

    WIP...

--- R/HRV.R
+++ R/HRV.R
@@ -1,8 +1,18 @@
-HRV <- function(workbook){
+HRV <- function(workbook,
+                max_estimated_r_peak_pct,
+                respiratory_peak_freq_range,
+                ){
 #
     # Flagging output files where bin length is too short (e.g., don't have at least 30 seconds of data)
+
+
+
+
     # Flagging segments where researcher has estimated more than 10% of R-peaks
     # Flagging segments where respiratory peak frequency is outside expected range
     # Flagging segments where heart rate is < twice respiratory rate
     # providing cross-referenced data for signals capturing # o/ cardiac cycles (ECG, ICG, BP) to catch missing beats/scoring errors
+
+
+
 }
--- R/IMP.R
+++ R/IMP.R
@@ -1,4 +1,4 @@
-ICG <- function(workbook){
+IMP <- function(workbook){
     #
     # Flag outliers (see above)
     # Back-up functions for calculating TPR
